d13fb5dd3fab18ba6ea211557fa6f7e99863535c
HeBomou <paakmau@outlook.com>
2020-03-25 21:10:38 +0800

    chore: Downgrade sass-loader version

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import cytoscape from "cytoscape"
 
 Vue.config.productionTip = false
+Vue.prototype.$cytoscape = cytoscape
 
 /* eslint-disable no-new */
 new Vue({
