c6024dac657234c23932dd8c25a668b352fd2be4
NJUleo <1250925329@qq.com>
2020-03-26 11:27:35 +0800

    feat(router):添加路由

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    }, {
+        path: '/login',
+        name: 'Login', 
+        component: () => import("@/views/login.vue"),
+        meta:{
+            title:"登陆",
+            keepAlive: true
+        }
+    }
+  ]
+})
