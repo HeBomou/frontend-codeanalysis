03fbe50bcb5eebab6604c125b2ae96716db143b1
HeBomou <paakmau@outlook.com>
2020-03-26 17:56:59 +0800

    chore: cytoscape hello world

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
 import router from './router'
+import cytoscape from 'cytoscape'
+
+Vue.prototype.$cytoscape = cytoscape
 
 Vue.config.productionTip = false
 
