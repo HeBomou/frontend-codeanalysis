c2bde459fed12bbececd5032041f145daa07a30d
HeBomou <paakmau@outlook.com>
2020-03-26 21:56:18 +0800

    chore: Install vuex and modify vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,39 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import HalouCao from '@/views/HalouCao'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'HalouCao',
-      component: HalouCao
-    }, {
-      path: '/login',
-      name: 'Login',
-      component: () => import("@/views/login.vue"),
-      meta: {
-        title: "登陆",
-        keepAlive: true
-      }
-    }, {
-      path: '/404',
-      name: 'faid',
-      component: () => import('@/views/404.vue'),
-      meta: {
-        title: "404",
-        keepAlive: true
-      }
-    }, {
-      path: '*',
-      redirect: "/404"
+const routes = [
+  {
+    path: '/',
+    name: 'HalouCao',
+    component: HalouCao
+  }, {
+    path: '/login',
+    name: 'Login',
+    component: () => import("@/views/login.vue"),
+    meta: {
+      title: "登陆",
+      keepAlive: true
     }
-  ]
+  }, {
+    path: '/404',
+    name: 'faid',
+    component: () => import('@/views/404.vue'),
+    meta: {
+      title: "404",
+      keepAlive: true
+    }
+  }, {
+    path: '*',
+    redirect: "/404"
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
 })
+
+export default router
