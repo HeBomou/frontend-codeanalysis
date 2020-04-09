4d868a952ca01aac3ab02e127676f2ee3a5e4ffd
NJUleo <1250925329@qq.com>
2020-04-09 19:25:54 +0800

    fix(login, adminLogin):top该成绝对像素数值

--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -248,7 +248,7 @@
         position: absolute;
         left: 0;
         margin-left: calc((100% - 317px) / 2);/* 里面的大小应该与这个表单的宽度相同，可以让他处于中间 */
-        top: calc((100% - 800px) / 2);
+        top: 40px;
         /*margin-top: calc( (100% - 600px) / 2);*/
     }
 
