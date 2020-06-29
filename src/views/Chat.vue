138e45540c34f4c23b6245532d38354f825f2f53
HeBomou <paakmau@outlook.com>
2020-06-29 18:09:44 +0800

    feat(chat): Improve appearance

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -5,34 +5,65 @@
       <!-- <v-spacer /> -->
     </v-app-bar>
     <v-content>
-      <v-container style="height: 100%;">
-        <v-row
-          style="margin-top: 5%; padding: 5px 20px 5px 20px; height: 80%; background: #FAF4ED;"
-        >
-          <v-col cols="3" style="height: 100%; overflow-y: scroll;">
-            <v-row justify="center" style="margin: 12px 0 12px 0;">联系人列表</v-row>
-            <v-card
-              v-for="item in people"
-              :key="item.id"
-              style="margin-bottom: 3px;"
-              height="80px"
-              color="#FFEFD5"
-            >
-              <v-list-item>
-                <v-list-item-content>
-                  <v-list-item-title class="headline mb-1">{{item.name}}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-card>
+      <v-container class="fill-height pa-0">
+        <v-row class="no-gutters elevation-4">
+          <v-col
+            cols="3"
+            class="flex-grow-1 flex-shrink-0"
+            style="border-right: 1px solid #0000001f;"
+          >
+            <v-responsive class="overflow-y-auto fill-height">
+              <v-list subheader>
+                <v-list-item-group>
+                  <template v-for="item in people">
+                    <v-list-item :key="item.id * 2" :value="item.id">
+                      <v-list-item-avatar color="grey lighten-2 white--text">
+                        <v-icon>mdi-chat</v-icon>
+                      </v-list-item-avatar>
+                      <v-list-item-content>
+                        <v-list-item-title v-text="item.name"></v-list-item-title>
+                      </v-list-item-content>
+                      <v-list-item-icon>
+                        <v-icon>mdi-chat</v-icon>
+                      </v-list-item-icon>
+                    </v-list-item>
+                    <v-divider :key="item.id * 2 + 1" class="my-0"></v-divider>
+                  </template>
+                </v-list-item-group>
+              </v-list>
+            </v-responsive>
           </v-col>
-          <v-col cols="9">
-            <v-row style="height: 59%; margin-bottom: 1%; background: #FFFEFE;"></v-row>
-            <v-row style="height: 30%; padding: 0 10px 0 10px;">
-              <v-textarea :value="inputText"></v-textarea>
-            </v-row>
-            <v-row justify="end" style="height: 10%; padding: 0 10px 0 10px;">
-              <v-btn>发送</v-btn>
-            </v-row>
+          <v-col cols="3" style="max-width: 100%;" class="flex-grow-1 flew-shrink-0">
+            <v-responsive class="overflow-y-auto fill-height" height="650">
+              <v-card flat class="d-flex flex-column fill-height">
+                <v-card-title>{{people[curPersonIndex].name}}</v-card-title>
+                <v-divider class="my-0"></v-divider>
+                <v-card-text class="flex-grow-1 overflow-y-auto">
+                  <template v-for="item in messages">
+                    <div :key="item.id" :class="{'d-flex flex-row-reverse': item.me}">
+                      <v-chip
+                        :color="item.me ? 'primary' : ''"
+                        dark
+                        class="pa-4 mb-2"
+                        style="height:auto; white-space:normal;"
+                      >{{item.text}}</v-chip>
+                    </div>
+                  </template>
+                </v-card-text>
+                <v-card-text class="flex-shrink-1">
+                  <v-text-field
+                    v-model="inputText"
+                    label="输入您的消息"
+                    type="text"
+                    no-details
+                    outlined
+                    append-outer-icon="mdi-send"
+                    @keyup.enter="sendMessage()"
+                    @click:append-outer="sendMessage()"
+                  ></v-text-field>
+                </v-card-text>
+              </v-card>
+            </v-responsive>
           </v-col>
         </v-row>
       </v-container>
@@ -50,13 +81,15 @@ export default {
       people: [
         { id: 1, name: "章鱼王" },
         { id: 2, name: "永不翘课" },
-        { id: 2, name: "胡子哥" }
+        { id: 3, name: "胡子哥" }
       ],
-      curPerson: 2,
+      curPersonIndex: 2,
       messages: [
-        { time: new Date(), text: "cnm" },
-        { time: new Date(), text: "cnm2" },
-        { time: new Date(), text: "cnm3" }
+        { id: 1, me: false, time: new Date(), text: "cnm" },
+        { id: 2, me: true, time: new Date(), text: "cnm2" },
+        { id: 3, me: false, time: new Date(), text: "cnm3" },
+        { id: 4, me: true, time: new Date(), text: "cnm4" },
+        { id: 5, me: false, time: new Date(), text: "cnm5" }
       ],
       inputText: ""
     };
@@ -83,6 +116,9 @@ export default {
     },
     onmessage(msg) {
       console.log("WS: 收到消息 ", msg.data);
+    },
+    sendMessage() {
+      console.log("Try to send msg: ", this.inputText);
     }
   }
 };
