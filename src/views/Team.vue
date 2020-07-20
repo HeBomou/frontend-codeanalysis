cac4ad407b32436719cc7537e35249ee241c7e0e
NJUleo <1250925329@qq.com>
2020-07-21 03:03:27 +0800

    fix(team):重写snack函数使其具有重载功能，提高复用性

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -4,40 +4,25 @@
       rel="stylesheet">
     <div>
         <v-snackbar
-        v-model="snackbar"
-        timeout="1000"
-        top
-        color="#5A7797"
-        rounded="pill"
-        >
-        {{ snackbarText }}
-
-        </v-snackbar>
-        <v-snackbar
             v-model="snack_alert"
             timeout="10000"
+            bottom
             color="#5A7797"
             rounded="pill"
-            bottom
         >
             {{ snack_alert_text }}
             <template v-slot:action="{ attrs }">
+              <div>
                 <v-btn
-                color="white"
-                text
-                v-bind="attrs"
-                @click="snack_alert = false;snack_alert_func()"
+                    v-for="(btn, i) in snack_alert_btns" :key="i"
+                    color="white"
+                    text
+                    v-bind="attrs"
+                    @click="snack_alert = false;btn.func()"
                 >
-                确定
+                {{btn.text}}
                 </v-btn>
-                <v-btn
-                color="white"
-                text
-                v-bind="attrs"
-                @click="snack_alert = false;snack_alert_func = () => {}"
-                >
-                取消
-                </v-btn>
+              </div>
             </template>
         </v-snackbar>
         <v-dialog
@@ -702,9 +687,6 @@ export default {
     },
     data(){
         return {
-            snackbarText: "",
-            
-            snackbar: false,
             overlay: true,
             haveNewChat: false,
             func1: API.getProjectBasicAttribute_group,
@@ -793,6 +775,11 @@ export default {
             menuDate: false,//date的menu是否打开
             snack_alert_text: "",
             snack_alert: false,
+            snack_alert_btns: [
+                {text: "确定", func: () => {}},
+                {text: "取消", func: () => {}},
+                {text: "取消", func: () => {}}
+            ]
         }
     },computed: {
       screenHeight() {
@@ -1150,14 +1137,16 @@ export default {
             this.getGroups();
         },onCopy(e){
             console.log(e);
-            this.snackbar = true;
-            this.snackbarText = "复制成功";
+            // this.snackbar = true;
+            // this.snackbarText = "复制成功";
+            this.snack("复制成功");
         },
         // 复制失败
         onErrorCopy(e){
             console.log(e);
-            this.snackbar = true;
-            this.snackbarText = "复制失败";
+            // this.snackbar = true;
+            // this.snackbarText = "复制失败";
+            this.snack("复制失败");
         },
         // 删除notice
         deleteNotice(notice){
@@ -1175,12 +1164,30 @@ export default {
             };
             this.snack("是否删除此公告？", delNot);
         },
-        snack(msg, func){
-            this.snack_alert_func = func;
+        snack(msg, btns){
+            if(typeof(btns) === "undefined"){
+                //只提示信息的情况(没有，非数组，数组长度为0)
+                this.snack_alert_btns = [{text: "确定", func: () => {}}];
+            }else if( typeof btns === "function"){
+                //只传入一个函数，当作是确认
+                this.snack_alert_btns = [{text: "确定", func: btns}, {text: "取消", func: () => {}}];
+            }else if(typeof btns == 'object' && btns.constructor == Array ){
+                //传入多个函数，对每个按钮有详细要求
+                this.snack_alert_btns = btns;
+            }else{
+                console.log("error in snack()");
+                this.snack_alert_btns = [{text: "确定", func: () => {}}];
+            }
             this.snack_alert_text = msg;
             this.snack_alert = true;
+
         },
-        snack_alert_func: () => {},
+        // snack(msg, func){
+        //     this.snack_alert_func = func;
+        //     this.snack_alert_text = msg;
+        //     this.snack_alert = true;
+        // },
+        // snack_alert_func: () => {},
 
         // isExecutor(user){
         //     if(user.id == 0){
--- src/views/try.vue
+++ src/views/try.vue
@@ -3,8 +3,10 @@
  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!-- <i class="material-icons">face</i> -->
       <v-btn @click="snack_alert = true">1</v-btn>
-      <v-btn @click="debug1()">2</v-btn>
-      <v-btn @click="debug2()">3</v-btn>
+      <v-btn @click="debug1()">1</v-btn>
+      <v-btn @click="debug2()">2</v-btn>
+      <v-btn @click="debug3()">3</v-btn>
+      <v-btn @click="debug4()">4</v-btn>
       <v-snackbar
             v-model="snack_alert"
             timeout="10000"
@@ -14,22 +16,17 @@
         >
             {{ snack_alert_text }}
             <template v-slot:action="{ attrs }">
+              <div>
                 <v-btn
+                 v-for="(btn, i) in snack_alert_btns" :key="i"
                 color="white"
                 text
                 v-bind="attrs"
-                @click="snack_alert = false;snack_alert_func()"
+                @click="snack_alert = false;btn.func()"
                 >
-                确定
+                {{btn.text}}
                 </v-btn>
-                <v-btn
-                color="white"
-                text
-                v-bind="attrs"
-                @click="snack_alert = false;snack_alert_func = () => {}"
-                >
-                取消
-                </v-btn>
+              </div>
             </template>
         </v-snackbar>
 
@@ -47,23 +44,45 @@ export default {
     return {
       snack_alert_text: "",
       snack_alert: false,
+      snack_alert_btns: [
+        {text: "确定", func: () => {}},
+        {text: "取消", func: () => {}},
+        {text: "取消", func: () => {}}
+      ]
     }
   },
   methods: {
     debug1(){
-      this.snack("111111111111111111111111111111111111111111111", () => {
-        console.log(1);
-      })
+      this.snack("haha");
     },
     debug2(){
       this.snack("2", () => {
         console.log(2);
       })
     },
-    snack(msg, func){
-            this.snack_alert_func = func;
-            this.snack_alert_text = msg;
-            this.snack_alert = true;
+    debug3(){
+      this.snack("3", [{text: "确定1", func: () => {console.log(3)}}, {text: "取消2", func: () => {console.log(4)}}]);
+    },
+    debug4(){
+      this.snack("4", 1);
+    },
+    snack(msg, btns){
+      if(typeof(btns) === "undefined"){
+        //只提示信息的情况(没有，非数组，数组长度为0)
+        this.snack_alert_btns = [{text: "确定", func: () => {}}];
+      }else if( typeof btns === "function"){
+        //只传入一个函数，当作是确认
+        this.snack_alert_btns = [{text: "确定", func: btns}, {text: "取消", func: () => {}}];
+      }else if(typeof btns == 'object' && btns.constructor == Array ){
+        //传入多个函数，对每个按钮有详细要求
+        this.snack_alert_btns = btns;
+      }else{
+        console.log("error in snack()");
+        this.snack_alert_btns = [{text: "确定", func: () => {}}];
+      }
+      this.snack_alert_text = msg;
+      this.snack_alert = true;
+
     },
     snack_alert_func: () => {},
   }
