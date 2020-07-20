100caf667491f15cded3b98ff49bac0e943ed484
NJUleo <1250925329@qq.com>
2020-07-21 04:33:53 +0800

    fix(projectComponent):修改alert为snackbar

--- src/components/ProjectComponent.vue
+++ src/components/ProjectComponent.vue
@@ -2,6 +2,28 @@
     <div>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
+      <v-snackbar
+            v-model="snack_alert"
+            timeout="10000"
+            bottom
+            color="#5A7797"
+            rounded="pill"
+        >
+            {{ snack_alert_text }}
+            <template v-slot:action="{ attrs }">
+              <div>
+                <v-btn
+                    v-for="(btn, i) in snack_alert_btns" :key="i"
+                    color="white"
+                    text
+                    v-bind="attrs"
+                    @click="snack_alert = false;btn.func()"
+                >
+                {{btn.text}}
+                </v-btn>
+              </div>
+            </template>
+        </v-snackbar>
         <v-dialog
         v-model="dialogErr"
         width="500">
@@ -222,8 +244,33 @@ export default {
             {text: "连通域数", value: "connectiveDomainNum"},
             {text: "详情", value: "id"},
             {text: "删除", value: "pid"}
+        ],
+        snack_alert_text: "",
+        snack_alert: false,
+        snack_alert_btns: [
+            {text: "确定", func: () => {}},
+            {text: "取消", func: () => {}},
+            {text: "取消", func: () => {}}
         ]
     }),methods:{
+        snack(msg, btns){
+            if(typeof(btns) === "undefined"){
+                //只提示信息的情况(没有，非数组，数组长度为0)
+                this.snack_alert_btns = [{text: "确定", func: () => {}}];
+            }else if( typeof btns === "function"){
+                //只传入一个函数，当作是确认
+                this.snack_alert_btns = [{text: "确定", func: btns}, {text: "取消", func: () => {}}];
+            }else if(typeof btns == 'object' && btns.constructor == Array ){
+                //传入多个函数，对每个按钮有详细要求
+                this.snack_alert_btns = btns;
+            }else{
+                console.log("error in snack()");
+                this.snack_alert_btns = [{text: "确定", func: () => {}}];
+            }
+            this.snack_alert_text = msg;
+            this.snack_alert = true;
+
+        },
         //获取所有得项目相关信息
         setProjects(id){
             this.isLoading = true;
@@ -254,12 +301,12 @@ export default {
             this.addProjectConfirmPressed = true;
             this.postProject(this.projectName, this.projectUrl, this.entityId).then(res => {
                 console.log(res);
-
                 this.dialog = false;
                 this.setProjects(this.entityId);
                 this.projectName = "";
                 this.projectUrl = "";
                 this.addProjectConfirmPressed = false;
+                this.snack("项目已提交至云端下载并解析");
             }).catch(err => {
                 this.addProjectConfirmPressed = false;
                 this.Alert(err.response.data.errMsg);
@@ -273,9 +320,11 @@ export default {
             //console.log(i);
             this.$store.commit("setProjectId",i);
             this.$router.push("/dependency");
-        },Alert(msg){
-        this.errMsg = msg;
-        this.dialogErr = true;
+        },
+        Alert(msg){
+        // this.errMsg = msg;
+        // this.dialogErr = true;
+            this.snack(msg);
         },
         deleteProject(i){
             //console.log(i);
