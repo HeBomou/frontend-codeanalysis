be5b15dc790ffa11381d3fbc7f342f1ddde8edb4
NJUleo <1250925329@qq.com>
2020-07-22 21:30:30 +0800

    feat(dialogAlert):html部分

--- src/components/DialogAlert.vue
+++ src/components/DialogAlert.vue
@@ -1,11 +1,25 @@
 <template>
   <v-slide-y-transition>
-    <v-dialog v-model="dialogErr" width="500">
-        <v-card justify="center">
-          <v-card-title>{{errMsg}}</v-card-title>
-          <v-card-text>
-            <v-btn color="error" @click="dialogConfirm">确定</v-btn>
-          </v-card-text>
+    <v-dialog v-model="dialog" width="500">
+        <v-card justify="center" color="#5A7797" class="white--text">
+          <v-card-title>{{msg}}</v-card-title>
+          <v-divider />
+          <v-card-actions>
+              <!-- <v-spacer v-if="i != 0"></v-spacer> -->
+              <v-btn 
+                :color="btns[0].color"
+                text
+              >
+                {{btns[0].text}}
+              </v-btn>
+              <v-spacer />
+              <v-btn 
+                :color="btns[1].color"
+                text
+              >
+                {{btns[1].text}}
+              </v-btn>
+          </v-card-actions>
         </v-card>
       </v-dialog>
   </v-slide-y-transition>
@@ -15,35 +29,31 @@ export default {
   name: "snackbar_alert_component",
   data() {
     return {
-      snack_alert_text: "",
-      snack_alert: false,
-      snack_alert_btns: [
-        { text: "确定", func: () => {} },
-        { text: "取消", func: () => {} },
-        { text: "取消", func: () => {} }
+      dialog: false,
+      msg: "",
+      btns: [
+        {text: "取消", func: () => {}, color: "err"}, 
+        {text: "确定", func: () => {}, color: "success"}
       ]
     };
   },
   methods: {
-    snack(msg, btns) {
-      if (typeof btns === "undefined") {
-        //只提示信息的情况(没有，非数组，数组长度为0)
-        this.snack_alert_btns = [{ text: "确定", func: () => {} }];
-      } else if (typeof btns === "function") {
-        //只传入一个函数，当作是确认
-        this.snack_alert_btns = [
-          { text: "确定", func: btns },
-          { text: "取消", func: () => {} }
-        ];
-      } else if (typeof btns == "object" && btns.constructor == Array) {
-        //传入多个函数，对每个按钮有详细要求
-        this.snack_alert_btns = btns;
+    Alert(msg, func1, func2) {
+      this.dialog = true;
+      if (!(typeof func1 === "function") || !(typeof func2 === "function")) {
+        console.log("error in alert dialog");
+      } else if(typeof func1 === "undefined"){
+        //没有传函数，认为只有一个按钮
+        console.log("2");
+        
+      } else if(typeof func2 === "undefined"){
+        //只有一个函数
+        console.log("3");
+
       } else {
-        console.log("error in snack()");
-        this.snack_alert_btns = [{ text: "确定", func: () => {} }];
+        console.log("4");
+
       }
-      this.snack_alert_text = msg;
-      this.snack_alert = true;
     }
   }
 };
--- src/views/try.vue
+++ src/views/try.vue
@@ -7,38 +7,67 @@
       <v-btn @click="debug3()">3</v-btn>
       <v-btn @click="debug4()">4</v-btn>
       <snack ref="ha"></snack>
+      <dialogAlert ref="dia"></dialogAlert>
+      <v-dialog v-model="dialog" width="500">
+        <v-card justify="center" color="#5A7797" class="white--text">
+          <v-card-title>{{msg}}</v-card-title>
+          <v-divider />
+          <v-card-actions>
+              <!-- <v-spacer v-if="i != 0"></v-spacer> -->
+              <v-btn 
+                :color="btns[0].color"
+                text
+              >
+                {{btns[0].text}}
+              </v-btn>
+              <v-spacer />
+              <v-btn 
+                :color="btns[1].color"
+                text
+              >
+                {{btns[1].text}}
+              </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
 
 </div>
 </template>
 
 <script>
 import snack from "../components/SnackbarAlert";
+import dialogAlert from "../components/DialogAlert";
 export default {
   name: "app",
 
   components: {
-   snack
+   snack,
+   dialogAlert
   },
   data(){
     return {
-      
+      dialog: false,
+      msg: "haha",
+      btns: [
+        {text: "取消", func: () => {}, color: "error"}, 
+        {text: "确定", func: () => {}, color: "success"}
+      ]
     }
   },
   methods: {
     debug1(){
       //this.snack("haha");
-      this.$refs.ha.snack("haha");
+      //this.$refs.ha.snack("haha");
+      this.$refs.dia.Alert("haha");
     },
     debug2(){
-      this.$refs.ha.snack("2", () => {
-        console.log(2);
-      })
+      this.dialog = true;
     },
     debug3(){
-      this.$refs.ha.snack("3", [{text: "确定1", func: () => {console.log(3)}}, {text: "取消2", func: () => {console.log(4)}}]);
+      
     },
     debug4(){
-      this.$refs.ha.snack("4", 1);
+
     },
     
   }
